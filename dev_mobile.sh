c6d17e7b9e88f580a287f8c10dbd0306260111e1
ki-za <xetarasteam@gmail.com>
2025-12-08 14:47:20 +0000

    mobile dev script (linux)

--- dev_mobile.sh
+++ dev_mobile.sh
@@ -1,42 +1,13 @@
-!# /bin/bash
-
-# auto detects my local ip
-# binds the hugo server to `0.0.0.0`
-# sets the `baseURL` to network accesible address
-
-# Colors for output
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-YELLOW='\033[1;33m'
-NC='\033[0m' # No Color
-
-echo "${BLUE}Bridget Mobile Development Server${NC}"
-echo ""
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  IP = $(hostname -I | awk '{print $1}')
-else
-  echo "Couldn't detect IP, likely not running linux-gnu"
-  IP = "localhost"
-fi
-
+#! /bin/bash
+ip=$(hostname -i | awk '{print $1}')
 PORT=1313
-
-echo "${GREEN} Mobile URL:${NC}"
-echo "${BLUE} https://${IP}:${PORT}${NC}"
-echo ""
 echo "Starting server ..."
 # start vite dev server
-
+pnpm run vite:dev &
+VITE_PID=$!
+# Start Hugo server with network binding
 hugo server --source=exampleSite --gc -D \
-  --disableFastRender --watch --logiLevel info \
+  --disableFastRender --watch --logLevel info \
   --bind 0.0.0.0 \
-  --baseURL http://${IP}:${PORT}
-
-trap "kill $VITE_PID 2>/dev/null/" EXIT
-
-# to make executable
-# chmod +x dev-mobile.sh
-#
-# run:
-# $./dev_mobile.sh
+  --baseURL http://${ip}:${PORT}
+trap "kill $VITE_PID 2>/dev/null" EXIT
